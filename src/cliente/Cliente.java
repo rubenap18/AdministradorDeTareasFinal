41bc342ea370b09480ea063fa673659c39bbb111
rubenap18 <itsrubenaguilar@gmail.com>
2025-08-16 13:36:25 -0700

    Added the whole code

--- src/cliente/Cliente.java
+++ src/cliente/Cliente.java
@@ -1,5 +1,312 @@
 package cliente;
 
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
 public class Cliente {
-    
+    public static void main(String[] args) {
+        
+        try {
+            // Cosas para establecer conexion al servidor
+            Socket socket = new Socket("192.168.140.12", 1800); // IP destino (computadora con el servidor)
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true); // Envia datos al cliente
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream())); // Lee datos enviados entre cliente y servidor
+            Scanner entrada = new Scanner(System.in);
+
+            System.out.println("-------------------------------------------");
+            System.out.println("| BIENVENIDO A ADMINISTRADOR DE PROYECTOS |");
+            System.out.println("-------------------------------------------");
+
+            boolean salir = false;
+            
+            // Bucle while para menu con salir validado
+
+            while (!salir) {
+                System.out.println("\n---> 1. Perfiles");
+                System.out.println("---> 2. Equipos");
+                System.out.println("---> 3. Proyectos");
+                System.out.println("---> 4. Tareas");
+                System.out.println("---> 5. Reportes");
+                System.out.println("---> 6. Salir");
+                System.out.print("-----> Ingresa una opción: ");
+                int opcion = entrada.nextInt();
+                entrada.nextLine();
+
+                switch (opcion) {
+                    case 1: menuPerfiles(out, in, entrada);     break; // Reciben los parametros requeridos para funcionar entre cliente y servidor
+                    case 2: menuEquipos(out, in, entrada); break;
+                    case 3: menuProyectos(out, in, entrada); break;
+                    case 4: menuTareas(out, in, entrada); break;
+                    case 5: menuReportes(out, in, entrada); break;
+                    case 6: salir = true; break;
+                    default: System.out.println("Opción no válida");
+                }
+            }
+            // Cerrar conexión
+            out.println("SALIR");
+            socket.close();
+            entrada.close();
+            System.out.println("Gracias por utilizar el programa");
+
+        } catch (IOException e) {
+            System.err.println("Error de conexión: " + e.getMessage());
+        }
+    }
+
+    // ---------------------- FUNCION PARA PERFILES ----------------------
+    static void menuPerfiles(PrintWriter out, BufferedReader in, Scanner entrada) throws IOException {
+        System.out.println("\n--- GESTIÓN DE PERFILES ---");
+        System.out.println("1. Crear perfil");
+        System.out.println("2. Ver perfiles");
+        System.out.println("3. Volver al menú principal");
+        System.out.print("Opción: ");
+        int opcion = entrada.nextInt();
+        entrada.nextLine();
+
+        switch (opcion) {
+            case 1:
+                System.out.print("Nombre del perfil: ");
+                String nombre = entrada.nextLine();
+
+                System.out.print("Rol: ");
+                String rol = entrada.nextLine();
+
+                out.println("CREAR_PERFIL|"+nombre+"|"+rol);
+                System.out.println(in.readLine());
+                break;
+
+            case 2:
+                out.println("LISTAR_PERFILES");
+                String respuesta = in.readLine();
+
+                if (respuesta.startsWith("ERROR")) {
+                    System.out.println(respuesta.split("\\|")[1]); // Separar el arreglo con simbolo
+
+                } else {
+                    System.out.println("\n--- LISTA DE PERFILES ---");
+                    String[] perfiles = respuesta.split(";");
+
+                    for (String perfil:perfiles) {
+                        String[] datos = perfil.split(",");
+                        System.out.println("ID: "+datos[0]+" | Nombre: "+datos[1]+" | Rol: "+datos[2]); // Ubicacion de los datos dentro de las matrices
+                    }
+                }
+                break;
+
+            case 3:
+                return;
+
+            default:
+                System.out.println("Opción no válida");
+        }
+    }
+
+    // ---------------------- FUNCION PARA EQUIPOS ----------------------
+    static void menuEquipos(PrintWriter out, BufferedReader in, Scanner entrada) throws IOException {
+        System.out.println("\n--- GESTIÓN DE EQUIPOS ---");
+        System.out.println("1. Crear equipo");
+        System.out.println("2. Listar equipos");
+        System.out.println("3. Volver al menú principal");
+        System.out.print("Opción: ");
+        int opcion = entrada.nextInt();
+        entrada.nextLine();
+
+        switch (opcion) {
+            case 1:
+                System.out.print("Nombre del equipo: ");
+                String nombre = entrada.nextLine();
+                out.println("CREAR_EQUIPO|" + nombre);
+                System.out.println(in.readLine());
+                break;
+
+            case 2:
+                out.println("LISTAR_EQUIPOS");
+                String respuesta = in.readLine();
+
+                if (respuesta.startsWith("ERROR")) {
+                    System.out.println(respuesta.split("\\|")[1]);
+
+                } else {
+                    System.out.println("\n--- LISTA DE EQUIPOS ---");
+                    String[] equipos = respuesta.split(";");
+
+                    for (String equipo:equipos) { // Bucle for each para mostrar los datos del arreglo especifico
+                        String[] datos = equipo.split(",");
+                        System.out.println("ID: " + datos[0] + " | Nombre: " + datos[1]);
+                    }
+                }
+                break;
+
+            case 3:
+                return;
+
+            default:
+                System.out.println("Opción no válida");
+        }
+    }
+
+    // ---------------------- FUNCION PARA PROYECTOS ----------------------
+    static void menuProyectos(PrintWriter out, BufferedReader in, Scanner entrada) throws IOException {
+        System.out.println("\n--- GESTIÓN DE PROYECTOS ---");
+        System.out.println("1. Crear proyecto");
+        System.out.println("2. Listar proyectos");
+        System.out.println("3. Asignar equipo a proyecto");
+        System.out.println("4. Volver al menú principal");
+        System.out.print("Opción: ");
+        int opcion = entrada.nextInt(); // Usuario digira opciones para que el switch case interactue
+        entrada.nextLine();
+
+        switch (opcion) {
+            case 1:
+                System.out.print("Nombre del proyecto: ");
+                String nombre = entrada.nextLine();
+                
+                out.println("CREAR_PROYECTO|" + nombre);
+                System.out.println(in.readLine());
+                break;
+
+            case 2:
+                out.println("LISTAR_PROYECTOS");
+                String respuesta = in.readLine();
+
+                if (respuesta.startsWith("ERROR")) {
+                    System.out.println(respuesta.split("\\|")[1]);
+
+                } else {
+                    System.out.println("\n--- LISTA DE PROYECTOS ---");
+                    String[] proyectos = respuesta.split(";");
+
+                    for (String proyecto : proyectos) {
+                        String[] datos = proyecto.split(","); // Cada que muestre los datos los separa por comas
+                        System.out.println("ID: " + datos[0] + 
+                                        " | Nombre: " + datos[1] + 
+                                        " | Equipo: " + (datos[2].equals("null") ? "Sin asignar" : datos[2]) + 
+                                        " | Estado: " + datos[3]);
+                    }
+                }
+                break;
+
+            case 3:
+                System.out.print("ID del proyecto: ");
+                int proyectoID = entrada.nextInt();
+
+                System.out.print("ID del equipo: ");
+                int equipoID = entrada.nextInt();
+                entrada.nextLine();
+
+                out.println("ASIGNAR_EQUIPO|" + proyectoID + "|" + equipoID); // Recibe los datos desde el servidor
+                System.out.println(in.readLine());
+                break;
+
+            case 4:
+                return;
+
+            default:
+                System.out.println("Opción no válida");
+        }
+    }
+
+    // ---------------------- FUNCION PARA TAREAS ----------------------
+    static void menuTareas(PrintWriter out, BufferedReader in, Scanner entrada) throws IOException {
+        System.out.println("\n--- GESTIÓN DE TAREAS ---");
+        System.out.println("1. Crear tarea");
+        System.out.println("2. Listar tareas");
+        System.out.println("3. Cambiar estado de tarea");
+        System.out.println("4. Volver al menú principal");
+        System.out.print("Opción: ");
+        int opcion = entrada.nextInt();
+        entrada.nextLine();
+
+        switch (opcion) {
+            case 1:
+                System.out.print("Título de la tarea: ");
+                String titulo = entrada.nextLine();
+
+                System.out.print("ID del proyecto: ");
+                int proyectoID = entrada.nextInt();
+                entrada.nextLine();
+
+                out.println("CREAR_TAREA|" + proyectoID + "|" + titulo);
+                System.out.println(in.readLine());
+                break;
+
+            case 2:
+                out.println("LISTAR_TAREAS");
+                String respuesta = in.readLine();
+
+                if (respuesta.startsWith("ERROR")) {
+                    System.out.println(respuesta.split("\\|")[1]);
+
+                } else {
+                    System.out.println("\n--- LISTA DE TAREAS ---");
+                    String[] tareas = respuesta.split(";"); // Dividir el vector por ;
+                    for (String tarea : tareas) {
+                        String[] datos = tarea.split(","); // Dividir el vector por ,
+                        System.out.println("ID: " + datos[0] + 
+                                    " | Proyecto: " + datos[1] +
+                                    " | Título: " + datos[2] + 
+                                    " | Estado: " + datos[3] +
+                                    " | Asignado a: " + datos[4] +
+                                    " | Avance: " + datos[5] + "%");
+                    }
+                }
+                break;
+
+            case 3:
+                System.out.print("ID de la tarea: ");
+                int tareaID = entrada.nextInt();
+                entrada.nextLine();
+
+                System.out.println("1. Pendiente");
+                System.out.println("2. En progreso");
+                System.out.println("3. Completado");
+                System.out.print("Nuevo estado: ");
+                int estado = entrada.nextInt();
+                entrada.nextLine();
+
+                out.println("CAMBIAR_ESTADO_TAREA|" + tareaID + "|" + estado); // Recibe los datos desde el servidor
+                System.out.println(in.readLine());
+                break;
+
+            case 4:
+                return;
+
+            default:
+                System.out.println("Opción no válida");
+        }
+    }
+
+    // ---------------------- FUNCION PARA REPORTES ----------------------
+    static void menuReportes(PrintWriter out, BufferedReader in, Scanner entrada) throws IOException {
+        System.out.println("\n--- REPORTES ---");
+        System.out.println("1. Avance de proyecto");
+        System.out.println("2. Tareas por estado");
+        System.out.println("3. Volver al menú principal");
+        System.out.print("Opción: ");
+        int opcion = entrada.nextInt();
+        entrada.nextLine();
+
+        switch(opcion){
+            case 1:
+                System.out.print("ID del proyecto: ");
+                int proyectoID = entrada.nextInt();
+                entrada.nextLine();
+
+                out.println("REPORTE_AVANCE|" + proyectoID); // Recibe el dato desde el servidor
+                System.out.println(in.readLine());
+                break;
+
+            case 2:
+                out.println("REPORTE_ESTADOS");
+                System.out.println(in.readLine());
+                break;
+
+            case 3:
+                return;
+
+            default:
+                System.out.println("Opción no válida");
+        }
+    }
 }
--- src/servidor/Servidor.java
+++ src/servidor/Servidor.java
@@ -1,5 +1,266 @@
 package servidor;
 
+import java.net.*;
+import java.io.*;
+
 public class Servidor {
-    
+    public static void main(String[] args) {
+        
+        // Almacenamiento de datos en servidor con matrices
+        String[][] perfiles = new String[100][3];  // ID, Nombre, Rol
+        String[][] equipos = new String[50][2];    // ID, Nombre
+        String[][] proyectos = new String[50][4];  // ID, Nombre, EquipoID, Estado
+        String[][] tareas = new String[500][6];   // ID, ProyectoID, Título, Estado, AsignadoA, Porcentaje
+        
+        int[] contadores = new int[4];  // Índices: 0=perfiles, 1=equipos, 2=proyectos, 3=tareas
+
+        try (ServerSocket serverSocket = new ServerSocket(1800)) {
+            System.out.println("Servidor esperando conexion");
+
+            while (true) {
+                
+                try (Socket clientSocket = serverSocket.accept();
+                    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
+
+                    System.out.println("Cliente conectado: "+clientSocket.getInetAddress());
+
+                    String comando;
+
+                    while ((comando = in.readLine()) != null) {
+                        System.out.println("Comando recibido: " + comando); // Muestra las acciones que se realizaron en el cliente
+                        String respuesta = procesarComando(  // Va a actuar dependiendo de la respuesta
+                            comando, 
+                            perfiles, 
+                            equipos, 
+                            proyectos, 
+                            tareas, 
+                            contadores
+                        );
+                        out.println(respuesta); // Envia lo que hace o la respuesta que obtuvo al cliente
+                    }
+                }catch(IOException e){
+                    System.err.println("Error con cliente: " + e.getMessage());
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error al iniciar servidor: " + e.getMessage());
+        }
+    }
+
+    public static String procesarComando(
+        String comando, 
+        String[][] perfiles,  // Recibe como parametro los arreglos
+        String[][] equipos, 
+        String[][] proyectos, 
+        String[][] tareas, 
+        int[] contadores
+    )  
+
+    {
+        String[] partes = comando.split("\\|"); // Extrae el primer elemento del arreglo que corresponde al comando
+        String tipoComando = partes[0];               // y los separa con el simbolo |
+
+        try {
+            switch (tipoComando) {
+                // ---- PERFILES ---- 
+                case "CREAR_PERFIL":
+                    if (partes.length != 3) return "ERROR | FORMATO INCORRECTO"; // Debe ser 3 porque matriz tiene 3 espacios (accion, nombre, rol)
+                    perfiles[contadores[0]][0] = String.valueOf(contadores[0]); // Convertir valor a String
+                    perfiles[contadores[0]][1] = partes[1];
+                    perfiles[contadores[0]][2] = partes[2];
+                    return "Perfil creado con ID: " + contadores[0]++;
+
+                case "LISTAR_PERFILES":
+                    if (contadores[0] == 0) return "ERROR | No hay perfiles registrados";
+                    StringBuilder sbPerfiles = new StringBuilder(); // Se crea para poder formatear la lista despues
+
+                    for (int i = 0; i < contadores[0]; i++) {
+                        sbPerfiles.append(String.join(",", perfiles[i]));
+                        if (i < contadores[0] - 1) sbPerfiles.append(";"); // Formatea la lista antes de enviarla al cliente
+                    }
+                    return "|" + sbPerfiles.toString();
+
+                // ---- EQUIPOS ----
+                case "CREAR_EQUIPO":
+                    if (partes.length != 2) return "ERROR | FORMATO INCORRECTO";
+                    equipos[contadores[1]][0] = String.valueOf(contadores[1]); // Convertir el valor int a String del contador
+                    equipos[contadores[1]][1] = partes[1];
+                    return "Equipo creado con ID: " + contadores[1]++;
+
+                case "LISTAR_EQUIPOS":
+                    if (contadores[1] == 0) return "ERROR | No hay equipos registrados";
+                    StringBuilder sbEquipos = new StringBuilder(); 
+
+                    for (int i = 0; i < contadores[1]; i++) {
+                        sbEquipos.append(String.join(",", equipos[i])); // Formatea la lista antes de enviarla al cliente
+                        if (i < contadores[1] - 1) sbEquipos.append(";");     // y reutilizar el arreglo de nuevo
+                    }
+                    return "|" + sbEquipos.toString();
+
+                // ---- PROYECTOS ----
+                case "CREAR_PROYECTO":
+                    if (partes.length != 2) return "ERROR | Formato: CREAR_PROYECTO|nombre"; // Valida que sea introducido correctamente (2 elementos)
+                    proyectos[contadores[2]][0] = String.valueOf(contadores[2]); // Convierte el valor digitado a String
+                    proyectos[contadores[2]][1] = partes[1]; // Nombre del proyecto
+                    proyectos[contadores[2]][2] = null; // Equipo asignado que al inicio es null
+                    proyectos[contadores[2]][3] = "Activo"; // Estado inicial
+                    return "OK|Proyecto creado con ID: " + contadores[2]++;
+
+                case "LISTAR_PROYECTOS":
+                    if (contadores[2] == 0) return "ERROR|No hay proyectos registrados";
+                    StringBuilder sbProyectos = new StringBuilder(); // Esto sirve para 
+
+                    for (int i = 0; i < contadores[2]; i++) {  // Bucle para buscar dentro del arreglo los datos del proyecto
+                        sbProyectos.append(proyectos[i][0] + "," + // Concatenar varios String
+                                        proyectos[i][1] + "," + // Ubicacion del nombre del proyecto
+                                        (proyectos[i][2] == null ? "null" : proyectos[i][2]) + "," + proyectos[i][3]); // Valida que haya un parametro en [i][2]
+                        if (i < contadores[2] - 1) sbProyectos.append(";");
+                    }
+                    return "|" + sbProyectos.toString(); // Muestra los datos creados convertidos a String
+
+                case "ASIGNAR_EQUIPO":
+                    if (partes.length != 3) return "ERROR|Formato: ASIGNAR_EQUIPO|proyectoID|equipoID";
+                    int proyectoID = Integer.parseInt(partes[1]); // Convierte de String a int
+                    int equipoID = Integer.parseInt(partes[2]); // Convierte de String a int
+                    
+                    if (proyectoID < 0 || proyectoID >= contadores[2]) // Valida si se introdujeron los datos necesarios
+                        return "ERROR | ID de proyecto no válido";
+                    if (equipoID < 0 || equipoID >= contadores[1]) 
+                        return "ERROR | ID de equipo no válido";
+                    
+                    proyectos[proyectoID][2] = partes[2];
+                    return "OK|Equipo asignado correctamente";
+
+                // ---- TAREAS ----
+                case "CREAR_TAREA":
+
+                
+                    if (partes.length != 3) return "ERROR | Formato incorrecto"; // Valida que el formato sea correctp (3 elementos)
+                    int proyectoIDTarea = Integer.parseInt(partes[1]);
+                    
+                    if (proyectoIDTarea < 0 || proyectoIDTarea >= contadores[2]) // Valida el ID del proyecto
+                        return "ERROR | ID de proyecto no válido";
+                    
+                     // Crea nuevas tareas con los valores por defecto
+                    tareas[contadores[3]][0] = String.valueOf(contadores[3]);
+                    tareas[contadores[3]][1] = partes[1];
+                    tareas[contadores[3]][2] = partes[2];
+                    tareas[contadores[3]][3] = "Pendiente";
+                    tareas[contadores[3]][4] = "Sin asignar";
+                    tareas[contadores[3]][5] = "0";
+                    return "| Tarea creada con ID: " + contadores[3]++;
+
+                    //
+                case "LISTAR_TAREAS":
+                    if (contadores[3] == 0) return "ERROR | No hay tareas registradas";
+                    StringBuilder sbTareas = new StringBuilder(); // Concatena cadenas de texto
+
+                    for (int i = 0; i < contadores[3]; i++) {
+                        String proyectoNombre = "ERROR | Proyecto no encontrado";
+
+                        // Obtener ID y nombre del proyecto desde el cliente
+                        try {
+                            int idProyecto = Integer.parseInt(tareas[i][1]); // Convierte String obtenido de ID a int
+                            proyectoNombre = proyectos[idProyecto][1]; // Obtiene el nombre del proyecto
+                        }catch(Exception e) {}
+                        
+                        //  Almacena los datos de la tarea en su espacio correspondiente
+                        sbTareas.append(tareas[i][0] + "," + 
+                                    proyectoNombre + "," + 
+                                    tareas[i][2] + "," + 
+                                    tareas[i][3] + "," + 
+                                    tareas[i][4] + "," + 
+                                    tareas[i][5]);
+                        if (i < contadores[3] - 1) sbTareas.append(";");
+                    }
+                    return "OK|" + sbTareas.toString(); //
+
+                case "CAMBIAR_ESTADO_TAREA":
+                    if (partes.length != 3) return "ERROR | Formato incorrecto"; // Valida el formato
+                    int tareaID = Integer.parseInt(partes[1]); // Convertir de String a int para almacenarlo de nuevo
+                    int estado = Integer.parseInt(partes[2]);
+                    
+                    if (tareaID < 0 || tareaID >= contadores[3]) // Valida que el ID sea correcto
+                        return "ERROR|ID de tarea no válido";
+                    
+                    String nuevoEstado = switch (estado) { // Cambia el estado de la tarea si se desea
+                        case 1 -> "Pendiente";
+                        case 2 -> "En progreso";
+                        case 3 -> "Completado";
+                        default -> tareas[tareaID][3];
+                    };
+                    
+                    tareas[tareaID][3] = nuevoEstado;
+                    return "|Estado actualizado a: " + nuevoEstado;
+
+                // ---- REPORTES ----
+                case "REPORTE_AVANCE":
+                    if (partes.length != 2) return "ERROR | Formato incorrecto"; // Valida que el formato sea correcto
+                    int proyectoIDReporte = Integer.parseInt(partes[1]);
+                    
+                    if (proyectoIDReporte < 0 || proyectoIDReporte >= contadores[2]) 
+                        return "ERROR | ID de proyecto no válido";
+                    
+                    int tareasPendientes = 0;
+                    int tareasProgreso = 0;
+                    int tareasCompletadas = 0;
+                    int totalTareas = 0;
+                    
+                    for (int i = 0; i < contadores[3]; i++){ // Actualiza el progreso de una tarea 
+
+                        // Comprueba la existencia de una tarea o el avance
+                        if (tareas[i][1] != null && Integer.parseInt(tareas[i][1]) == proyectoIDReporte){ 
+
+                            totalTareas++;
+
+                            switch (tareas[i][3]) { // Entra a la ubicacion de los estados de las tareas y los va contando
+                                case "Pendiente": tareasPendientes++; break;
+                                case "En progreso": tareasProgreso++; break;
+                                case "Completado": tareasCompletadas++; break;
+                            }
+                        }
+                    }
+                    
+                    if (totalTareas == 0) return "El proyecto no tiene tareas";
+                    
+                    double porcentaje = (tareasCompletadas * 100.0) / totalTareas; // Calculo del porcentaje de avance
+                    return "|Proyecto: " + proyectos[proyectoIDReporte][1] + 
+                        "|Tareas totales: " + totalTareas + 
+                        "|Pendientes: " + tareasPendientes + 
+                        "|En progreso: " + tareasProgreso + 
+                        "|Completadas: " + tareasCompletadas + 
+                           "|Porcentaje: " + String.format("%.2f", porcentaje) + "%"; //Sacamos solo 2 decimales del valor decimal que calcula
+
+                case "REPORTE_ESTADOS":
+                    if (contadores[3] == 0) return "No hay tareas registradas";
+                    
+                    // Inicia contadores para cada estado de tareas y mostrarlos luego
+                    int pendientes = 0;
+                    int progreso = 0;
+                    int completadas = 0;
+                    
+                    for (int i = 0; i < contadores[3]; i++) { // Lee arreglo y acumula las tareas dependiendo de su estado
+                        switch (tareas[i][3]) {
+                            case "Pendiente": pendientes++; break;
+                            case "En progreso": progreso++; break;
+                            case "Completado": completadas++; break;
+                        }
+                    }
+                    
+                    return "|Pendientes: " + pendientes + 
+                        "|En progreso: " + progreso + 
+                        "|Completadas: " + completadas + 
+                        "|Total: " + contadores[3];
+
+                case "SALIR":
+                    return "Servidor sigue activo";
+
+                default:
+                    return "ERROR | Comando desconocido";
+            }
+        } catch (Exception e) {
+            return "ERROR | Error procesando comando: " + e.getMessage();
+        }
+    }
 }
